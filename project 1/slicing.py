f3950fa95401b27467c85662ef7d63b631fedbcf
d3ads3c <ovelordplay80@gmail.com>
2019-05-10 12:29:56 +0330

    first project - slice

---
+++ project 1/slicing.py
@@ -0,0 +1,6 @@
+new_list = [15, 20, 68, 48, 10]
+print(new_list[0::2])
+print(new_list[-1::-1])
+poem = "hello darkness my old friend i've come to talk with you again"
+word = poem[::-1]
+print(word)
