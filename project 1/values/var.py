903a124b79618c70dd598dfadba3154596555b6a
d3ads3c <ovelordplay80@gmail.com>
2019-05-10 11:57:27 +0330

    first project - commit new

---
+++ project 1/values/var.py
@@ -0,0 +1,16 @@
+number1 = 52
+number2 = 1.2
+text = "hello world"
+print(text)
+print(type(text))
+# list
+my_list = ["hello", 15, 95.3, True]
+print(my_list[0])
+
+
+my_tuple = ("hello", 65, True)
+print(my_tuple[-1])
+
+# boolean
+error_ = False
+print(error_)
