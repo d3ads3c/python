f6c36afe3bacd65408c6da144229146cf4b429ab
d3ads3c <overlordplay80@gmail.com>
2019-05-17 10:04:58 +0330

    method

---
+++ project2/methods.py
@@ -0,0 +1,13 @@
+# capital word
+text = "hello world\n"
+text_1 = text.capitalize()
+text_2 = text.upper()
+print(text_1, text_2)
+
+# tuper count values
+tup = (1, 2, 3, 4, 4, 4)
+tup_count = tup.count(4)
+print(tup_count)
+
+# finding index in tup
+
---
+++ project2/operator.py
@@ -0,0 +1,7 @@
+# print 3time "Hello world"
+text ="hello world"
+text_dup = text * 3
+print(text_dup)
+# ------------
+list = [1, 2, 3]
+list_new = [4, 5, 6]
