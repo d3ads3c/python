90b68c2457686a8c8e764a5cc9d9bb39a1d36d4f
d3ads3c <overlordplay80@gmail.com>
2019-07-26 10:35:49 +0330

    bmi app

---
+++ Project 9/bmi.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask('my application')
+
+
+@app.route('/', methods=["GET", "POST"])
+def main_page():
+    if request.method == 'GET':
+        return render_template("index.html")
+    else:
+        weight = float(request.form['w'])
+        height = float(request.form['h'])
+        bmi = weight / height ** 2
+        bmi_preview = f"{bmi:.2f}"
+
+        return render_template("index.html", bmi=bmi, bmi_preview=bmi_preview)
+
+
+app.run()
